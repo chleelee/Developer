a99ea68775999e972f048d107392258603ee0d7d
chleelee <leeching@holine.com>
2021-12-04 10:27:17 +0800

    VSCode

--- src/com/minnesta/edu/info/manager/domain/Student.java
+++ src/com/minnesta/edu/info/manager/domain/Student.java
@@ -2,5 +2,13 @@ package com.minnesta.edu.info.manager.domain;
 
 public class Student extends Person{
 
+    public Student() {
+    }
+
+    public Student(String id, String name, String age, String addr){
+        super(id, name, age, addr);
+    }
+    
+
 
 }
--- src/com/minnesta/edu/info/manager/domain/Teacher.java
+++ src/com/minnesta/edu/info/manager/domain/Teacher.java
@@ -1,6 +1,13 @@
 package com.minnesta.edu.info.manager.domain;
 
 public class Teacher extends Person{
+
+    public Teacher() {
+    }
+
+    public Teacher(String id, String name, String age, String addr) {
+        super(id, name, age, addr);
+    }
     
 
 }
